bafac3883aac63fc4eb0457890990898c23da11f
Yang Guo <jimmy_guoyang@outlook.com>
2025-04-11 22:51:30 +0800

    fix: add license

--- fluss-client/src/test/java/com/alibaba/fluss/client/table/FlussLogITCase.java
+++ fluss-client/src/test/java/com/alibaba/fluss/client/table/FlussLogITCase.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2025 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alibaba.fluss.client.table;
 
 import com.alibaba.fluss.client.Connection;
@@ -15,7 +31,6 @@ import com.alibaba.fluss.metadata.TableDescriptor;
 import com.alibaba.fluss.metadata.TablePath;
 import com.alibaba.fluss.server.testutils.FlussClusterExtension;
 import com.alibaba.fluss.types.DataTypes;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
