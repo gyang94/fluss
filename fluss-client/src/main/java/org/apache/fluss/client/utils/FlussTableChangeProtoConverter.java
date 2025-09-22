f7f5e49714649250bee7c0574d9406fdafbd528b
Yang Guo <jimmy_guoyang@outlook.com>
2025-09-22 22:35:12 +0800

    feat: remove enum in proto

--- fluss-client/src/main/java/org/apache/fluss/client/utils/FlussTableChangeProtoConverter.java
+++ fluss-client/src/main/java/org/apache/fluss/client/utils/FlussTableChangeProtoConverter.java
@@ -20,9 +20,6 @@ package org.apache.fluss.client.utils;
 import org.apache.fluss.metadata.AlterTableConfigsOpType;
 import org.apache.fluss.metadata.TableChange;
 import org.apache.fluss.rpc.messages.PbAlterConfigsRequestInfo;
-import org.apache.fluss.rpc.messages.PbFlussTableChange;
-import org.apache.fluss.rpc.messages.PbResetOption;
-import org.apache.fluss.rpc.messages.PbSetOption;
 
 /** Convert {@link TableChange} to proto. */
 public class FlussTableChangeProtoConverter {
@@ -43,33 +40,5 @@ public class FlussTableChangeProtoConverter {
                     "Unsupported table change: " + tableChange.getClass());
         }
         return info;
-    }
-
-    public static PbFlussTableChange toProto(TableChange tableChange) {
-        PbFlussTableChange proto = new PbFlussTableChange();
-        if (tableChange instanceof TableChange.SetOption) {
-            PbSetOption pbSetOption = toPbSetOption((TableChange.SetOption) tableChange);
-            proto.setChangeType(PbFlussTableChange.ChangeType.SET_OPTION);
-            proto.setSetOption(pbSetOption);
-        } else if (tableChange instanceof TableChange.ResetOption) {
-            PbResetOption pbResetOption = toPbResetOption((TableChange.ResetOption) tableChange);
-            proto.setChangeType(PbFlussTableChange.ChangeType.RESET_OPTION);
-            proto.setResetOption(pbResetOption);
-        }
-
-        return proto;
-    }
-
-    private static PbSetOption toPbSetOption(TableChange.SetOption setOption) {
-        PbSetOption pbSetOption = new PbSetOption();
-        pbSetOption.setKey(setOption.getKey());
-        pbSetOption.setValue(setOption.getValue());
-        return pbSetOption;
-    }
-
-    private static PbResetOption toPbResetOption(TableChange.ResetOption resetOption) {
-        PbResetOption pbResetOption = new PbResetOption();
-        pbResetOption.setKey(resetOption.getKey());
-        return pbResetOption;
     }
 }
